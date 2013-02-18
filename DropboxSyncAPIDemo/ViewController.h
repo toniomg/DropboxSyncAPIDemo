7c4cb041fa73aadcbbc45ce470fba2da2c98b97d
Antonio M <tonio.mg@gmail.com>
2013-02-18 16:24:19 +0000

    Added account link

--- DropboxSyncAPIDemo/ViewController.h
+++ DropboxSyncAPIDemo/ViewController.h
@@ -10,4 +10,6 @@
 
 @interface ViewController : UIViewController
 
+-(IBAction)linkAccountPressed:(id)sender;
+
 @end
--- DropboxSyncAPIDemo/ViewController.m
+++ DropboxSyncAPIDemo/ViewController.m
@@ -8,22 +8,49 @@
 
 #import "ViewController.h"
 
+#import <Dropbox/Dropbox.h>
+
 @interface ViewController ()
+
+@property (strong, nonatomic) NSDictionary * folderContent;
 
 @end
 
 @implementation ViewController
 
+@synthesize folderContent = _folderContent;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+	[[DBAccountManager sharedManager] linkFromController:self];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Dropbox Sync API
+
+-(void)listContent{
+    
+    NSMutableDictionary *folderContentMutable = [[NSMutableDictionary alloc] init];
+    
+    NSArray *contents = [[DBFilesystem sharedFilesystem] listFolder:[DBPath root] error:nil];
+    for (DBFileInfo *info in contents) {
+        [folderContentMutable setObject:@"1" forKey:info.path];
+    }
+    
+    self.folderContent = [[NSDictionary alloc] initWithDictionary:folderContentMutable];
+
+}
+#pragma mark - Actions
+
+-(IBAction)linkAccountPressed:(id)sender {
+    [[DBAccountManager sharedManager] linkFromController:self];
 }
 
 @end
